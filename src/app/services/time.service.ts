a1fb3ccee7dddc87a17545e23c2666a2216ecebc
Jeet <jeet7420@gmail.com>
2019-10-28 01:08:49 +0530

    Buy Sell WIP

--- src/app/services/time.service.ts
+++ src/app/services/time.service.ts
@@ -42,45 +42,72 @@ export class TimeService {
     this.hourDiff = duration.asHours();
     this.minDiff = duration.asMinutes();
 
-    if(this.minDiff>0) {
-      this.minDiff = this.minDiff + 1;
-      if(this.minDiff==60) {
-        this.minDiff = 0;
-        this.hourCarryOver = 1;
-      }
+     console.log('day diff : ', this.dayDiff); 
+     console.log('hour diff : ' , this.hourDiff); 
+     console.log('min diff : ' , this.minDiff); 
+
+     console.log('FLOOR');
+
+     console.log('day diff : ' , Math.floor(this.dayDiff)); 
+     console.log('hour diff : ' , Math.floor(this.hourDiff)); 
+     console.log('min diff : ' , Math.floor(this.minDiff)); 
+
+    // if(this.minDiff>0) {
+    //   this.minDiff = this.minDiff + 1;
+    //   if(this.minDiff==60) {
+    //     this.minDiff = 0;
+    //     this.hourCarryOver = 1;
+    //   }
+    // }
+
+    // if(this.hourDiff>0) {
+    //   this.hourDiff = this.hourDiff + 1;
+    //   if(this.hourDiff==24) {
+    //     this.hourDiff = 0;
+    //     this.dayCarryOver = 1;
+    //   }
+    // }
+    // this.hourDiff = this.hourDiff + this.hourCarryOver;
+
+    // this.dayDiff = this.dayDiff + this.dayCarryOver;
+
+    // if(this.dayDiff>0) {
+    //   this.hourDiff = this.hourDiff + this.dayDiff * 24;
+    // }
+
+    // if(this.hourDiff<10) {
+    //   this.durationInHours = '0' + this.hourDiff;
+    // }
+    // else {
+    //   this.durationInHours = + this.hourDiff;
+    // }
+
+    // if(this.minDiff<10) {
+    //   this.durationInMins = '0' + this.minDiff;
+    // }
+    // else {
+    //   this.durationInMins = + this.minDiff;
+    // }
+    // //console.log('duration : ' , this.durationInHours); 
+    // //console.log('hours : ' , this.durationInMins); 
+    // //console.log('minutes : ' , duration.asMinutes()); 
+    // this.duration = { durationInHour: this.durationInHours , durationInMin: this.durationInMins };
+
+    this.durationInHours = Math.floor(this.hourDiff);
+    this.minDiff = Math.floor(this.minDiff) - (Math.floor(this.hourDiff) * 60) + 1;
+    this.durationInMins = Math.floor(this.minDiff);
+
+    if(parseInt(this.durationInHours)<10) {
+      this.durationInHours = '0' + this.durationInHours;
     }
 
-    if(this.hourDiff>0) {
-      this.hourDiff = this.hourDiff + 1;
-      if(this.hourDiff==24) {
-        this.hourDiff = 0;
-        this.dayCarryOver = 1;
-      }
-    }
-    this.hourDiff = this.hourDiff + this.hourCarryOver;
-
-    this.dayDiff = this.dayDiff + this.dayCarryOver;
-
-    if(this.dayDiff>0) {
-      this.hourDiff = this.hourDiff + this.dayDiff * 24;
+    if(parseInt(this.durationInMins)<10) {
+      this.durationInMins = '0' + this.durationInMins;
     }
 
-    if(this.hourDiff<10) {
-      this.durationInHours = '0' + this.hourDiff;
-    }
-    else {
-      this.durationInHours = + this.hourDiff;
-    }
+    console.log('duration in hours : ' , this.durationInHours);
+    console.log('duration in mins : ' , this.durationInMins);
 
-    if(this.minDiff<10) {
-      this.durationInMins = '0' + this.minDiff;
-    }
-    else {
-      this.durationInMins = + this.minDiff;
-    }
-    console.log('duration : ' , this.durationInHours); 
-    console.log('hours : ' , this.durationInMins); 
-    //console.log('minutes : ' , duration.asMinutes()); 
     this.duration = { durationInHour: this.durationInHours , durationInMin: this.durationInMins };
 
     return this.duration;
