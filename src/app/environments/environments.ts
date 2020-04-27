8ff993b5e953ee0233e5a2cf14356fc162ef754c
shekhar_kony <shekhar.suman@kony.com>
2020-04-28 00:59:24 +0530

    Added IPs in environment

--- src/app/environments/environments.ts
+++ src/app/environments/environments.ts
@@ -3,50 +3,54 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-    production: false
-  };
+  production: false
+};
 
-  export const ADMIN_ROLE = 'Admin';
-  export const USER_ROLE = 'User';
+export const devAddress = "http://139.59.30.90:8080";
+export const uatAddress = "http://139.59.34.247:8080";
 
-  export const ACTION_FORECAST = 'FORECAST';
-  export const ACTION_CREATE = 'CREATE';
-  export const ACTION_EDIT = 'EDIT';
-  
-  export const SUPPORT_NUMBER = '';
-  
-  export const INGRESS_URL = 'http://139.59.30.90:8080/et_ingress/rest';
-  export const ADMIN_URL = 'http://139.59.30.90:8080/et_order/rest/';
-  export const ORDER_URL = 'http://139.59.30.90:8080/et_order/rest/';
-  export const FORECAST_URL = 'http://139.59.30.90:8080/et_forecast/rest/';
-  export const TRADE_URL = 'http://139.59.30.90:3000/apiv1/trade';
+export const activeBackend = uatAddress;
 
+export const ADMIN_ROLE = 'Admin';
+export const USER_ROLE = 'User';
 
-  
-  
-  export const DEV_FLAG = true;
-  export const NO_OTP_FLAG = true;
-  export const PRIVACY_POLICY_URL = 'https://docs.google.com/document/d/1e45gQ3-Ri6Eri9QM07hmCYfVfZfTj1ek0Iq1QGjYwxs/edit'
-  export const TERMS_OF_USE_URL = "https://docs.google.com/document/d/1e45gQ3-Ri6Eri9QM07hmCYfVfZfTj1ek0Iq1QGjYwxs/edit";
-  
-  
- 
-  
-  
+export const ACTION_FORECAST = 'FORECAST';
+export const ACTION_CREATE = 'CREATE';
+export const ACTION_EDIT = 'EDIT';
 
-  
+export const SUPPORT_NUMBER = '';
+
+export const INGRESS_URL = activeBackend + '/et_ingress/rest';
+export const ADMIN_URL = activeBackend + '/et_order/rest/';
+export const ORDER_URL = activeBackend + '/et_order/rest/';
+export const FORECAST_URL = activeBackend + '/et_forecast/rest/';
+export const TRADE_URL = activeBackend + '/apiv1/trade';
 
 
-  export const ENABLE_SERVICES = true;
-  
-  
-  /*
-   * For easier debugging in development mode, you can import the following file
-   * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
-   *
-   * This import should be commented out in production mode because it will have a negative impact
-   * on performance if an error is thrown.
-   */
+
+
+export const DEV_FLAG = true;
+export const NO_OTP_FLAG = true;
+export const PRIVACY_POLICY_URL = 'https://docs.google.com/document/d/1e45gQ3-Ri6Eri9QM07hmCYfVfZfTj1ek0Iq1QGjYwxs/edit'
+export const TERMS_OF_USE_URL = "https://docs.google.com/document/d/1e45gQ3-Ri6Eri9QM07hmCYfVfZfTj1ek0Iq1QGjYwxs/edit";
+
+
+
+
+
+
+
+
+
+export const ENABLE_SERVICES = true;
+
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
   // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
-  
-  
+
